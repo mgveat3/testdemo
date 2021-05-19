633e16f85d572ba7f4cdb1f67767434f25218507
mgveat3 <qq745917578a1@163.com>
2021-05-19 15:24:41 +0800

    我是bb==

--- TextDemo/TextDemo/AController.m
+++ TextDemo/TextDemo/AController.m
@@ -16,10 +16,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    NSLog(@"我是aa==");
-   
+
+    NSLog(@"我是bb===");
 }
 
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 @end
