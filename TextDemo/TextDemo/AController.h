cf7dcf7fbcebcbf44253f8cebf65e8b967c4fed7
mgveat3 <qq745917578a1@163.com>
2021-05-19 14:50:28 +0800

    update

---
+++ TextDemo/TextDemo/AController.h
@@ -0,0 +1,13 @@
+//
+//  AController.h
+//  TextDemo
+//
+//  Created by bing on 2017/5/25.
+//  Copyright © 2017年 Bing_John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AController : UIViewController
+
+@end
