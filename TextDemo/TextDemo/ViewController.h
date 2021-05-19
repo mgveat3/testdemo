7cbce88adab6ca3837b6a05bf87d180ed3742e61
mgveat3 <qq745917578a1@163.com>
2021-05-19 14:08:29 +0800

    初始化项目

---
+++ TextDemo/TextDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextDemo
+//
+//  Created by bing on 2017/5/25.
+//  Copyright © 2017年 Bing_John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TextDemo/TextDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TextDemo
+//
+//  Created by bing on 2017/5/25.
+//  Copyright © 2017年 Bing_John. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
